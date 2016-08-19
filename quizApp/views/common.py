8a38bf2cb7ed3585d9dee66c83d5ef804803df86
Alexei Bendebury <alyosha.be@gmail.com>
2016-08-19 16:22:17 -0700

    Further refactoring

--- quizApp/views/common.py
+++ quizApp/views/common.py
@@ -1,8 +1,9 @@
 """Common objects for the views.
 """
 import pdb
-from flask import render_template, jsonify, request
+from flask import render_template, jsonify, request, url_for
 from flask.views import MethodView
+from quizApp import db
 
 
 class ObjectListView(MethodView):
@@ -56,3 +57,50 @@ class ObjectListView(MethodView):
         record.save()
 
         return jsonify({"success": 1, "next_url": self.member_url(record)})
+
+class ObjectView(MethodView):
+    @property
+    def model(self):
+        """The model this collection operates on.
+        """
+        raise NotImplementedError
+
+    @property
+    def update_form(self):
+        """A form used for updating models that this collection will
+        operate on.
+        """
+        raise NotImplementedError
+
+    def collection_url(self, **kwargs):
+        """Return url that leads to the collection of such objects.
+        """
+        raise NotImplementedError
+
+    def get_record(self, **kwargs):
+        """Given a bunch of url parameters, get the model they specify,
+        validating as necessary.
+        """
+        raise NotImplementedError
+
+    def put(self, **kwargs):
+        record = self.get_record(**kwargs)
+        update_record_form = self.update_form(request.form)
+
+        if not update_record_form.validate():
+            return jsonify({"success": 0, "errors":
+                            update_record_form.errors})
+
+        update_record_form.populate_obj(record)
+        db.session.commit()
+        return jsonify({"success": 1})
+
+    def delete(self, **kwargs):
+        record = self.get_record(**kwargs)
+        db.session.delete(record)
+        db.session.commit()
+
+        return jsonify({"success": 1,
+                        "next_url": self.collection_url(**kwargs)})
+
+
--- quizApp/views/datasets.py
+++ quizApp/views/datasets.py
@@ -13,7 +13,7 @@ from quizApp.forms.common import DeleteObjectForm, ObjectTypeForm
 from quizApp.forms.datasets import DatasetForm, GraphForm
 from quizApp.models import Dataset, MediaItem
 from quizApp.views.helpers import validate_model_id, validate_form_or_error
-from quizApp.views.common import ObjectListView
+from quizApp.views.common import ObjectListView, ObjectView
 
 datasets = Blueprint("datasets", __name__, url_prefix="/datasets")
 
@@ -40,69 +40,44 @@ class DatasetListView(ObjectListView):
 datasets.add_url_rule('/', view_func=DatasetListView.as_view("datasets"))
 
 
-class DatasetView(MethodView):
+class DatasetView(ObjectView):
     """Views for manipulating a DataSet.
     """
     decorators = [roles_required("experimenter")]
+    update_form = DatasetForm
+    model = Dataset
 
-    def put(self, dataset_id):
-        """Change the properties of this dataset.
-        """
-        dataset = validate_model_id(Dataset, dataset_id)
+    def collection_url(self, **kwargs):
+        return url_for("datasets.datasets")
 
-        update_dataset_form = DatasetForm(request.form)
+    def get_record(self, dataset_id):
+        return validate_model_id(Dataset, dataset_id)
 
-        if not update_dataset_form.validate():
-            return jsonify({"success": 0, "errors":
-                            update_dataset_form.errors})
-
-        update_dataset_form.populate_obj(dataset)
-
-        db.session.commit()
-
-        return jsonify({"success": 1})
-
-    def delete(self, dataset_id):
-        """Delete this dataset.
-        """
-        dataset = validate_model_id(Dataset, dataset_id)
-
-        db.session.delete(dataset)
-        db.session.commit()
-
-        return jsonify({"success": 1,
-                        "next_url": url_for('datasets.datasets')})
 
 datasets.add_url_rule(DATASET_ROUTE, view_func=DatasetView.as_view("dataset"))
 
 
-class MediaItemView(MethodView):
+class MediaItemView(ObjectView):
     """Views for manipulating a MediaItem.
     """
     decorators = [roles_required("experimenter")]
+    def collection_url(self, dataset_id, **kwargs):
+        return url_for("datasets.settings_dataset", dataset_id=dataset_id)
 
-    def delete(self, dataset_id, media_item_id):
-        """Delete a particular media_item in a particular dataset.
-        """
+    def get_record(self, dataset_id, media_item_id):
         dataset = validate_model_id(Dataset, dataset_id)
         media_item = validate_model_id(MediaItem, media_item_id)
 
         if media_item not in dataset.media_items:
             abort(404)
 
-        db.session.delete(media_item)
-        db.session.commit()
+        return media_item
 
-        return jsonify({"success": 1})
 
     def get(self, dataset_id, media_item_id):
         """Get an html representation of a particular media_item.
         """
-        dataset = validate_model_id(Dataset, dataset_id)
-        media_item = validate_model_id(MediaItem, media_item_id)
-
-        if media_item not in dataset.media_items:
-            abort(404)
+        media_item = self.get_record(dataset_id, media_item_id)
 
         return render_template("datasets/read_media_item.html",
                                media_item=media_item)
@@ -112,16 +87,12 @@ class MediaItemView(MethodView):
 
         Dispatches to a handler for the specific kind of media item.
         """
-        dataset = validate_model_id(Dataset, dataset_id)
-        media_item = validate_model_id(MediaItem, media_item_id)
-
-        if media_item not in dataset.media_items:
-            abort(404)
+        media_item = self.get_record(dataset_id, media_item_id)
 
         if media_item.type == "graph":
-            return self.update_graph(dataset, media_item)
+            return self.update_graph(media_item)
 
-    def update_graph(self, _, graph):
+    def update_graph(self, graph):
         """Update a graph.
         """
         update_graph_form = GraphForm(CombinedMultiDict((request.form,
@@ -157,28 +128,6 @@ class MediaItemView(MethodView):
 datasets.add_url_rule(MEDIA_ITEM_ROUTE,
                       view_func=MediaItemView.as_view("media_item"))
 
-
-@datasets.route(DATASET_ROUTE + '/settings', methods=["GET"])
-@roles_required("experimenter")
-def settings_dataset(dataset_id):
-    """View the configuration of a particular dataset.
-    """
-    dataset = validate_model_id(Dataset, dataset_id)
-
-    update_dataset_form = DatasetForm(obj=dataset)
-
-    delete_dataset_form = DeleteObjectForm()
-
-    create_media_item_form = ObjectTypeForm()
-    create_media_item_form.populate_object_type(MEDIA_ITEM_TYPES)
-
-    return render_template("datasets/settings_dataset.html",
-                           dataset=dataset,
-                           update_dataset_form=update_dataset_form,
-                           delete_dataset_form=delete_dataset_form,
-                           create_media_item_form=create_media_item_form)
-
-
 @datasets.route(MEDIA_ITEMS_ROUTE, methods=["POST"])
 @roles_required("experimenter")
 def create_media_item(dataset_id):
@@ -200,6 +149,27 @@ def create_media_item(dataset_id):
     return jsonify({
         "success": 1,
     })
+
+
+@datasets.route(DATASET_ROUTE + '/settings', methods=["GET"])
+@roles_required("experimenter")
+def settings_dataset(dataset_id):
+    """View the configuration of a particular dataset.
+    """
+    dataset = validate_model_id(Dataset, dataset_id)
+
+    update_dataset_form = DatasetForm(obj=dataset)
+
+    delete_dataset_form = DeleteObjectForm()
+
+    create_media_item_form = ObjectTypeForm()
+    create_media_item_form.populate_object_type(MEDIA_ITEM_TYPES)
+
+    return render_template("datasets/settings_dataset.html",
+                           dataset=dataset,
+                           update_dataset_form=update_dataset_form,
+                           delete_dataset_form=delete_dataset_form,
+                           create_media_item_form=create_media_item_form)
 
 
 @datasets.route(MEDIA_ITEM_ROUTE + "/settings", methods=["GET"])
