e31448ea758fc11de973c76b85b889df89fa9b0f
Alexei Bendebury <alyosha.be@gmail.com>
2016-08-25 18:03:37 -0700

    Datasets use ObjectCollectionView now

--- quizApp/views/datasets.py
+++ quizApp/views/datasets.py
@@ -12,6 +12,7 @@ from quizApp.forms.common import DeleteObjectForm, ObjectTypeForm
 from quizApp.forms.datasets import DatasetForm, GraphForm, TextForm
 from quizApp.models import Dataset, MediaItem
 from quizApp.views.helpers import validate_model_id, validate_form_or_error
+from quizApp.views.common import ObjectCollectionView
 
 datasets = Blueprint("datasets", __name__, url_prefix="/datasets")
 
@@ -24,40 +25,29 @@ MEDIA_ITEMS_ROUTE = os.path.join(DATASET_ROUTE + "/media_items/")
 MEDIA_ITEM_ROUTE = os.path.join(MEDIA_ITEMS_ROUTE + "<int:media_item_id>")
 
 
-@datasets.route("/", methods=["GET"])
-@roles_required("experimenter")
-def read_datasets():
-    """Display a list of all datasets.
+class DatasetCollectionView(ObjectCollectionView):
+    """View for managing a collection of Datasets.
     """
-    datasets_list = Dataset.query.all()
-    create_dataset_form = DatasetForm()
-    confirm_delete_dataset_form = DeleteObjectForm()
+    decorators = [roles_required("experimenter")]
+    methods = ["GET", "POST"]
+    resolve_kwargs = dict
+    template = "datasets/read_datasets.html"
 
-    return render_template(
-        "datasets/read_datasets.html",
-        datasets=datasets_list,
-        confirm_delete_dataset_form=confirm_delete_dataset_form,
-        create_dataset_form=create_dataset_form)
+    def create_form(self):
+        return DatasetForm(request.form)
 
+    def get_members(self):
+        return Dataset.query.all()
 
-@datasets.route("/", methods=["POST"])
-@roles_required("experimenter")
-def create_dataset():
-    """Create a new dataset.
-    """
-    create_dataset_form = DatasetForm(request.form)
+    def create_member(self, create_form):
+        dataset = Dataset()
+        create_form.populate_obj(dataset)
+        dataset.save()
+        return {"next_url": url_for("datasets.settings_dataset",
+                                    dataset_id=dataset.id)}
 
-    if not create_dataset_form.validate():
-        return jsonify({"success": 0, "errors": create_dataset_form.errors})
-
-    dataset = Dataset()
-    create_dataset_form.populate_obj(dataset)
-
-    dataset.save()
-
-    return jsonify({"success": 1,
-                    "next_url": url_for("datasets.settings_dataset",
-                                        dataset_id=dataset.id)})
+datasets.add_url_rule("/",
+                      view_func=DatasetCollectionView.as_view('datasets'))
 
 
 @datasets.route(DATASET_ROUTE, methods=["PUT"])
@@ -90,7 +80,7 @@ def delete_dataset(dataset_id):
     db.session.commit()
 
     return jsonify({"success": 1,
-                    "next_url": url_for('datasets.read_datasets')})
+                    "next_url": url_for('datasets.datasets')})
 
 
 @datasets.route(MEDIA_ITEM_ROUTE, methods=["DELETE"])
@@ -146,30 +136,36 @@ def settings_dataset(dataset_id):
         create_media_item_form=create_media_item_form)
 
 
-@datasets.route(MEDIA_ITEMS_ROUTE, methods=["POST"])
-@roles_required("experimenter")
-def create_media_item(dataset_id):
-    """Create a new media item.
+class MediaItemCollectionView(ObjectCollectionView):
+    """Manage a collection of MediaItems.
     """
-    dataset = validate_model_id(Dataset, dataset_id)
-    create_media_item_form = ObjectTypeForm()
-    create_media_item_form.populate_object_type(MEDIA_ITEM_TYPES)
+    decorators = [roles_required("experimenter")]
+    methods = ["POST"]
+    get_members = None
+    template = None
 
-    response = validate_form_or_error(create_media_item_form)
+    def resolve_kwargs(self, dataset_id):
+        dataset = validate_model_id(Dataset, dataset_id)
+        return {"dataset": dataset}
 
-    if response:
-        return response
+    def create_form(self):
+        create_media_item_form = ObjectTypeForm()
+        create_media_item_form.populate_object_type(MEDIA_ITEM_TYPES)
+        return create_media_item_form
 
-    media_item = MediaItem(type=create_media_item_form.object_type.data,
-                           dataset=dataset)
-    media_item.save()
+    def create_member(self, create_form, dataset):
+        media_item = MediaItem(type=create_form.object_type.data,
+                               dataset=dataset)
+        media_item.save()
 
-    return jsonify({
-        "success": 1,
-        "next_url": url_for("datasets.settings_media_item",
-                            dataset_id=dataset.id,
-                            media_item_id=media_item.id),
-    })
+        return {
+            "next_url": url_for("datasets.settings_media_item",
+                                dataset_id=dataset.id,
+                                media_item_id=media_item.id),
+        }
+
+datasets.add_url_rule(MEDIA_ITEMS_ROUTE,
+                      view_func=MediaItemCollectionView.as_view('media_items'))
 
 
 @datasets.route(MEDIA_ITEM_ROUTE + "/settings", methods=["GET"])
