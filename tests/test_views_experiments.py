c4ae2e8dd1f77cd6e9a9f9fd28135fbb3830ab4d
Alexei Bendebury <alyosha.be@gmail.com>
2016-09-08 13:03:59 -0700

    More testing

--- tests/test_views_experiments.py
+++ tests/test_views_experiments.py
@@ -294,6 +294,16 @@ def test_read_assignment(client, users):
         assert response.status_code == 200
         assert question.question in data
 
+    experiment.stop = datetime.now() - timedelta(days=1)
+
+    for assignment in assignment_set.assignments:
+        # Make sure we can't read it
+        question = assignment.activity
+        response = client.get(url + str(assignment.id))
+        assert response.status_code == 400
+
+    experiment.stop = datetime.now() + timedelta(days=1)
+
     experiment.disable_previous = True
     db.session.commit()
 
@@ -426,6 +436,14 @@ def test_update_assignment(client, users):
 
     assert response.status_code == 200
     assert json_success(response.data)
+
+    # Don't let them update old experiments
+    experiment.stop = datetime.now() - timedelta(days=1)
+    response = client.patch(url,
+                            data={"choices": choice.id}
+                            )
+    assert response.status_code == 400
+    experiment.stop = datetime.now() + timedelta(days=1)
 
     response = client.patch(url)
 
