fb1afb9c3302c1fc41b78cd9b944640e1050d742
Alexei Bendebury <alyosha.be@gmail.com>
2016-09-15 16:08:51 -0700

    Merged migrations together

---
+++ migrations/versions/99a92884f8bf_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 99a92884f8bf
+Revises: ('20fbb13ea446', '1eef7a613153')
+Create Date: 2016-09-15 16:07:34.591096
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '99a92884f8bf'
+down_revision = ('20fbb13ea446', '1eef7a613153')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
